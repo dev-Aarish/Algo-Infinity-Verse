d6ea60f243e6e2a4a3fa1e52de87229efecd74e8
Aritro Bag <aritrobag362@gmail.com>
2026-07-19 11:20:09 +0530

    feat: added copy to clipboard button for code snippets

--- pages/express-academy/express-academy.js
+++ pages/express-academy/express-academy.js
@@ -409,6 +409,7 @@ function renderLesson(lesson) {
     window.eli5Toggle.initToggle('express', DOM.tabLesson);
   }
   const btn = document.getElementById('mark-lesson-complete');
+   copyCode.init(DOM.tabLesson);
   if (!isCompleted) {
     btn.addEventListener('click', () => {
       markItemComplete(lesson.id);
--- pages/typescript-academy/typescript-academy.js
+++ pages/typescript-academy/typescript-academy.js
@@ -1325,6 +1325,7 @@ function renderLesson(lesson) {
     window.eli5Toggle.initToggle('typescript', DOM.tabLesson);
   }
     const btn = document.getElementById('mark-lesson-complete');
+    copyCode.init(DOM.tabLesson);
     if (!isCompleted) {
         btn.addEventListener('click', () => {
             markItemComplete(lesson.id);
